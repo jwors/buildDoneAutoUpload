e67a93ca4c69265f5e6a7cd675d2fec3451243fc
我可真猛啊 <j.wors@outlook.com>
2022-06-30 23:00:45 +0800

    补充

--- src/buildDoneAutoPlugin/buildDoneAutoPlugin.ts
+++ src/buildDoneAutoPlugin/buildDoneAutoPlugin.ts
@@ -1,6 +1,25 @@
+interface options {
+    account: String,
+    password:String,
+    serverUrl:Number,
+    port:Number,
+    localPath:String,
+    remotePath:String,
+}
+const sftp = require('ssh2-sftp-client')
+const sftp = new sftp()
 class BuildDoneAutoPlugin {
-    constructor() {
-
+    /** 
+    *@account {String} 服务器账号
+    @password {password} 服务器密码
+    @serverUrl {Number} 服务器地址
+    @port {Number} 端口
+    @localPath {String} 本地build-dist之后的地址
+    @remotePath {String} 远端目的文件地址
+    */
+    options: options
+    constructor(options:options) {
+        this.options = options
     }
 }
 
