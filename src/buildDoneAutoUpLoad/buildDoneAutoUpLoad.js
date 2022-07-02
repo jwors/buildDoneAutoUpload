7da1f4d90da3c9020f45dc2e37aad0b1af596fa1
我可真猛啊 <j.wors@outlook.com>
2022-07-03 00:11:14 +0800

    补充参数注释，补充test

--- src/buildDoneAutoUpLoad/buildDoneAutoUpLoad.js
+++ src/buildDoneAutoUpLoad/buildDoneAutoUpLoad.js
@@ -2,20 +2,21 @@ const sftp = require('ssh2-sftp-client')
 const path = require('path')
 const upLoad = new sftp()
 const DingHint = require('../dingHint/dingHint.js')
-// demo
+
 class BuildDoneAutoUpLoad {
     /** 
-     *@param {String} options.serverAccount 服务器账号
-     *@param {password}  options.serverPassword 服务器密码
-     *@param {Number} options.serverUrl 服务器地址
-     *@param {Number} options.port 端口
-     *@param {String} localPath  本地build-dist之后的地址
-     *@param {String}  remotePath 远端目的文件地址
-     *@param {String}  projectName 项目名称
-     *@param {String}  text  钉钉提示内容
-     * @param {string} webHookUrl - 钉钉推送地址
-     * @param {string} text - 提示内容
-     * @param {string | number } keyWord - 机器人关键字
+     *@param {Object} options - 整个参数
+     *@param {String} options.serverAccount- 服务器账号
+     *@param {password}  options.serverPassword - 服务器密码
+     *@param {Number} options.serverUrl - 服务器地址
+     *@param {Number} options.port - 端口
+     *@param {String} options.localPath - 本地build-dist之后的地址
+     *@param {String} options.remotePath - 远端目的文件地址
+     *@param {String} options.projectName - 项目名称
+     *@param {String} options.text - 钉钉提示内容
+     * @param {string} options.webHookUrl - 钉钉推送地址
+     * @param {string} options.text - 提示内容
+     * @param {string | number } options.keyWord - 机器人关键字
      */
     constructor(options) {
         this.options = options
